32d97b408c869206c6920952781345a6b1b4b3ee
Viet To Hoang <viet.to@idealogic.com.vn>
2022-09-21 17:51:20 +0700

    First Init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using SlientSpyBot;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
